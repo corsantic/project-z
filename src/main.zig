75491ce220adc08dca7ad559bcf4ed86443d64ee
corsantic <aydofurkan25@gmail.com>
2025-08-07 01:29:09 +0300

    changing structs etc about player

--- src/main.zig
+++ src/main.zig
@@ -12,22 +12,27 @@ pub fn main() !void {
     // can change these for new board
     const cell_count: u8 = 9;
     const board_size: u8 = 3;
-    var board = try Board.init(allocator, cell_count, board_size);
+    const first_player = Player.init("Player 1", "*", PlayerOrder.first);
+    const second_player = Player.init("Player 2", "#", PlayerOrder.second);
+
+    // Initialize board with first player
+    var board = try Board.init(allocator, cell_count, board_size, &first_player);
     defer board.deinit();
 
     const stdin = std.io.getStdIn().reader();
     // stdout is a std.io.Writer
     const stdout = std.io.getStdOut().writer();
 
-    board.draw();
-    try processInput(stdin, stdout, &board);
+    board.draw(&first_player, &second_player);
+    try processInput(stdin, stdout, &board, &first_player, &second_player);
 }
 
-fn processInput(stdin: anytype, stdout: anytype, board: *Board) !void {
+fn processInput(stdin: anytype, stdout: anytype, board: *Board, first_player: *const Player, second_player: *const Player) !void {
     var i: i32 = 0;
+
     while (true) : (i += 1) {
         var buf: [10]u8 = undefined;
-        try stdout.print("{any} - Please enter your number: ", .{board.current_player});
+        try stdout.print("{s} - Please enter your number: ", .{board.current_player.name});
 
         if (try stdin.readUntilDelimiterOrEof(&buf, '\n')) |line| {
             var number = line;
@@ -43,24 +48,24 @@ fn processInput(stdin: anytype, stdout: anytype, board: *Board) !void {
             const parsed = std.fmt.parseInt(u8, number, 10) catch null;
 
             if (parsed) |num| {
-                std.debug.print("{any}\n", .{num});
+                // std.debug.print("{any}\n", .{num});
                 for (board.cells) |*cell| {
                     if (cell.order == num and cell.player == null) {
                         cell.player = board.current_player;
-                        board.changePlayer();
+                        board.changePlayer(first_player, second_player);
                         break;
                     }
                 }
             }
         }
-        board.draw();
+        board.draw(first_player, second_player);
     }
 }
 pub const Board = struct {
     cells: []Cell,
     size: u8,
-    current_player: Player = .first, // Player whose turn it is
-    status: GameStatus = .draw, // Current game status
+    current_player: *const Player, // Player whose turn it is
+    status: GameStatus, // Current game status
     allocator: Allocator,
 
     const Self = @This();
@@ -69,29 +74,29 @@ pub const Board = struct {
         self.allocator.free(self.cells);
     }
 
-    fn init(allocator: Allocator, cell_count: u8, size: u8) !Self {
+    fn init(allocator: Allocator, cell_count: u8, size: u8, player: *const Player) !Self {
         const cells = try allocator.alloc(Cell, cell_count);
         for (cells, 0..) |*cell, i| {
             cell.* = Cell{ .order = @intCast(i + 1) };
         }
         return Board{
             .cells = cells,
-            .current_player = .first,
-            .status = .draw,
+            .current_player = player,
+            .status = GameStatus.draw,
             .size = size,
             .allocator = allocator,
         };
     }
 
-    fn draw(self: *Self) void {
+    fn draw(self: *Self, first_player: *const Player, second_player: *const Player) void {
         for (self.cells) |cell| {
             var buf: [5]u8 = undefined;
             var cell_value: []const u8 = std.fmt.bufPrint(&buf, "{}", .{cell.order}) catch "err";
 
-            if (cell.player == Player.first) {
-                cell_value = "*";
-            } else if (cell.player == Player.second) {
-                cell_value = "#";
+            if (cell.player == first_player) {
+                cell_value = first_player.symbol;
+            } else if (cell.player == second_player) {
+                cell_value = second_player.symbol;
             }
 
             if (cell.order % self.size == 0) {
@@ -103,30 +108,33 @@ pub const Board = struct {
             }
         }
     }
-    fn changePlayer(self: *Self) void {
-        if (self.current_player == Player.first) {
-            self.current_player = Player.second;
+    fn changePlayer(self: *Self, first_player: *const Player, second_player: *const Player) void {
+        if (self.current_player == first_player) {
+            self.current_player = second_player;
         } else {
-            self.current_player = Player.first;
+            self.current_player = first_player;
         }
     }
 };
 pub const Cell = struct {
     order: u8 = 0, // Order of the cell in the game (1-9)
-    player: ?Player = null, // Player who occupies the cell, null if empty
-    // allocator: Allocator,
-    // const Self = @This();
-
-    // fn init(allocator: Allocator, order: u8) !Self {
-    //     return Cell{ .order = order, .player = null, .allocator = allocator };
-    // }
-    // fn deinit(self: *Self) void {
-    //     // If the cell was allocated, free it
-    //     self.allocator.free(self);
-    // }
+    player: ?*const Player = null, // Player who occupies the cell, null if empty
 };
 
-pub const Player = enum {
+pub const Player = struct {
+    name: []const u8, // Name of the player
+    symbol: []const u8, // Symbol used by the player
+    player: PlayerOrder, // Order of the player in the game
+
+    fn init(name: []const u8, symbol: []const u8, player_order: PlayerOrder) Player {
+        return Player{
+            .name = name,
+            .symbol = symbol,
+            .player = player_order,
+        };
+    }
+};
+pub const PlayerOrder = enum {
     first,
     second,
 };
